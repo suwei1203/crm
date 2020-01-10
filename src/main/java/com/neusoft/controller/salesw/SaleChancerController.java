e919b149f2a061d4db61d5d8db376136c87d41f3
suwei1203 <59653446+suwei1203@users.noreply.github.com>
2020-01-10 22:26:43 +0800

    Update SaleChancerController.java

--- src/main/java/com/neusoft/controller/salesw/SaleChancerController.java
+++ src/main/java/com/neusoft/controller/salesw/SaleChancerController.java
@@ -1,5 +1,7 @@
 package com.neusoft.controller.salesw;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,6 +41,10 @@ public class SaleChancerController {
 		return saleChanceService.delSaleChance(saleChance);
 	}
 	
+	@RequestMapping("/selectSaleChancePaging")
+	public List<SaleChance> selectSaleChancePaging(@RequestBody SaleChance saleChance){
+		return saleChanceService.selectSaleChancePaging(saleChance);
+	}
 	
 	
 	
