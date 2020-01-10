8e022bb9716da788588ce010a94654469bed20a7
suwei1203 <59653446+suwei1203@users.noreply.github.com>
2020-01-10 22:27:08 +0800

    Update SaleChanceService.java

--- src/main/java/com/neusoft/service/salesw/SaleChanceService.java
+++ src/main/java/com/neusoft/service/salesw/SaleChanceService.java
@@ -1,5 +1,7 @@
 package com.neusoft.service.salesw;
 
+import java.util.List;
+
 import com.neusoft.entity.SaleChance;
 
 public interface SaleChanceService {
@@ -13,4 +15,6 @@ public interface SaleChanceService {
 	public int updateSaleChance(SaleChance saleChance);
 	
 	public int delSaleChance(SaleChance saleChance);
+	
+	public List<SaleChance> selectSaleChancePaging(SaleChance saleChance);
 }
