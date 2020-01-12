f49552017759f77af980cf5dca8318f630b8ca57
suwei1203 <59653446+suwei1203@users.noreply.github.com>
2020-01-12 22:33:06 +0800

    Update SaleChanceMapper.java

--- src/main/java/com/neusoft/mapper/salesw/SaleChanceMapper.java
+++ src/main/java/com/neusoft/mapper/salesw/SaleChanceMapper.java
@@ -10,19 +10,20 @@ import com.neusoft.entity.SaleChance;
 
 @Mapper
 public interface SaleChanceMapper {
-	
+
 	public int insertSaleChance(SaleChance saleChance);
-	
-	@Select("select count(*) from  saleChance")
-	public int selectSaleChanceCount();
-	
+
+	public int selectSaleChanceCount(SaleChance saleChance);
+
 	@Select("select * from  saleChance where chanceId=#{chanceId}")
-	public SaleChance selectSaleChanceCountByChanceId(SaleChance saleChance);
-	
+	public SaleChance selectSaleChanceByChanceId(SaleChance saleChance);
+
 	public int updateSaleChance(SaleChance saleChance);
-	
+
 	@Delete("delete from saleChance where chanceId=#{chanceId}")
 	public int delSaleChance(SaleChance saleChance);
-	
+
 	public List<SaleChance> selectSaleChancePaging(SaleChance saleChance);
+
+	public int selectLastChanceId();
 }
