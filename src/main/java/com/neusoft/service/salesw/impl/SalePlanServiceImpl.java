28d375ff2a8c6efa5890462754fc09080515af91
suwei1203 <59653446+suwei1203@users.noreply.github.com>
2020-01-13 15:06:58 +0800

    Create SalePlanServiceImpl.java

---
+++ src/main/java/com/neusoft/service/salesw/impl/SalePlanServiceImpl.java
@@ -0,0 +1,28 @@
+package com.neusoft.service.salesw.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.neusoft.entity.SalePlan;
+import com.neusoft.mapper.salesw.SalePlanMapper;
+import com.neusoft.service.salesw.SalePlanService;
+
+@Service
+public class SalePlanServiceImpl implements SalePlanService {
+
+	@Autowired
+	SalePlanMapper salePlanMapper;
+	
+	@Override
+	public int insertSalePlan(SalePlan SalePlan) {
+		return salePlanMapper.insertSalePlan(SalePlan);
+	}
+	
+	@Override
+	public List<SalePlan> selectSalePlanByPlanChcId(SalePlan SalePlan){
+		return salePlanMapper.selectSalePlanByPlanChcId(SalePlan);
+	}
+
+}
