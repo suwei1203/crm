a3955926ef2951e4a8fced73f8faa0787af2522c
suwei1203 <59653446+suwei1203@users.noreply.github.com>
2020-01-13 15:05:55 +0800

    Create SalePlanController.java

---
+++ src/main/java/com/neusoft/controller/salesw/SalePlanController.java
@@ -0,0 +1,29 @@
+package com.neusoft.controller.salesw;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.neusoft.entity.SalePlan;
+import com.neusoft.service.salesw.SalePlanService;
+
+@RestController
+public class SalePlanController {
+
+	@Autowired
+	SalePlanService salePlanService;
+
+	@RequestMapping("/insertSalePlan")
+	public int insertSalePlan(@RequestBody SalePlan SalePlan) {
+		return salePlanService.insertSalePlan(SalePlan);
+	}
+	
+	@RequestMapping("/selectSalePlanByPlanChcId")
+	public List<SalePlan> selectSalePlanByPlanChcId(@RequestBody SalePlan SalePlan){
+		return salePlanService.selectSalePlanByPlanChcId(SalePlan);
+	}
+
+}
