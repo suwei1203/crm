bfee238c59e3655370f39d592274c3bfeec6224f
suwei1203 <59653446+suwei1203@users.noreply.github.com>
2020-01-14 17:07:57 +0800

    Create ClientInfoService.java

---
+++ src/main/java/com/neusoft/service/salesw/ClientInfoService.java
@@ -0,0 +1,9 @@
+package com.neusoft.service.salesw;
+
+import com.neusoft.entity.ClientInfo;
+
+public interface ClientInfoService {
+	
+	//添加客户信息
+	public int insertClientInfo(ClientInfo clientInfo);
+}
