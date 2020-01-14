85088f2e0ac562bf305e91eb8b324310748bb656
suwei1203 <59653446+suwei1203@users.noreply.github.com>
2020-01-14 14:32:11 +0800

    Create ClientLinkman.java

---
+++ src/main/java/com/neusoft/entity/ClientLinkman.java
@@ -0,0 +1,71 @@
+package com.neusoft.entity;
+
+public class ClientLinkman {
+	//客户联系人编号
+	private Integer clientLinkmanId;
+	//客户联系人姓名
+	private String clientLinkmanName;
+	//客户联系人性别
+	private Integer clientLinkmanSex;
+	//客户联系人职位
+	private String clientLinkmanJob;
+	//客户联系人办公电话
+	private String clientLinkmanTel;
+	//客户联系人手机
+	private String clientLinkmanMobile;
+	//备注
+	private String remark;
+	//客户编号
+	private String clientCod;
+	
+	public Integer getClientLinkmanId() {
+		return clientLinkmanId;
+	}
+	public void setClientLinkmanId(Integer clientLinkmanId) {
+		this.clientLinkmanId = clientLinkmanId;
+	}
+	public String getClientLinkmanName() {
+		return clientLinkmanName;
+	}
+	public void setClientLinkmanName(String clientLinkmanName) {
+		this.clientLinkmanName = clientLinkmanName;
+	}
+	public Integer getClientLinkmanSex() {
+		return clientLinkmanSex;
+	}
+	public void setClientLinkmanSex(Integer clientLinkmanSex) {
+		this.clientLinkmanSex = clientLinkmanSex;
+	}
+	public String getClientLinkmanJob() {
+		return clientLinkmanJob;
+	}
+	public void setClientLinkmanJob(String clientLinkmanJob) {
+		this.clientLinkmanJob = clientLinkmanJob;
+	}
+	public String getClientLinkmanTel() {
+		return clientLinkmanTel;
+	}
+	public void setClientLinkmanTel(String clientLinkmanTel) {
+		this.clientLinkmanTel = clientLinkmanTel;
+	}
+	public String getClientLinkmanMobile() {
+		return clientLinkmanMobile;
+	}
+	public void setClientLinkmanMobile(String clientLinkmanMobile) {
+		this.clientLinkmanMobile = clientLinkmanMobile;
+	}
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+	public String getClientCod() {
+		return clientCod;
+	}
+	public void setClientCod(String clientCod) {
+		this.clientCod = clientCod;
+	}
+	
+	
+}
