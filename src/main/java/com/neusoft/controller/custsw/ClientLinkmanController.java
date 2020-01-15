f45e8658c4e7793cd37906d7af6e3e252b7e5458
suwei1203 <59653446+suwei1203@users.noreply.github.com>
2020-01-15 16:56:58 +0800

    Update ClientLinkmanController.java

--- src/main/java/com/neusoft/controller/custsw/ClientLinkmanController.java
+++ src/main/java/com/neusoft/controller/custsw/ClientLinkmanController.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.neusoft.entity.ClientLinkman;
 import com.neusoft.service.custsw.ClientLinkmanService;
 
+@RestController
 public class ClientLinkmanController {
 	@Autowired
 	ClientLinkmanService clientLinkmanService;
