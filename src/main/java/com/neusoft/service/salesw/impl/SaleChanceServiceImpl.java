2356f9cfd6f1766e4d24e76435dba7342cb80a1f
suwei1203 <59653446+suwei1203@users.noreply.github.com>
2020-01-15 13:17:04 +0800

    Update SaleChanceServiceImpl.java

--- src/main/java/com/neusoft/service/salesw/impl/SaleChanceServiceImpl.java
+++ src/main/java/com/neusoft/service/salesw/impl/SaleChanceServiceImpl.java
@@ -15,36 +15,43 @@ public class SaleChanceServiceImpl implements SaleChanceService  {
 	@Autowired
 	SaleChanceMapper saleChanceMapper;
 	
+	//添加销售机会
 	@Override
 	public int insertSaleChance(SaleChance saleChance) {
 		return saleChanceMapper.insertSaleChance(saleChance);
 	}
 	
+	// 按条件查询销售机会总行数
 	@Override
 	public int selectSaleChanceCount(SaleChance saleChance) {
 		return saleChanceMapper.selectSaleChanceCount(saleChance);
 	}
 	
-	@Override
-	public int updateSaleChance(SaleChance saleChance) {
-		return saleChanceMapper.updateSaleChance(saleChance);
-	}
-	
+	// 按ID查询销售机会
 	@Override
 	public SaleChance selectSaleChanceByChanceId(SaleChance saleChance) {
 		return saleChanceMapper.selectSaleChanceByChanceId(saleChance);
 	}
 	
+	//修改销售机会
+	@Override
+	public int updateSaleChance(SaleChance saleChance) {
+		return saleChanceMapper.updateSaleChance(saleChance);
+	}
+	
+	//删除销售机会
 	@Override
 	public int delSaleChance(SaleChance saleChance) {
 		return saleChanceMapper.delSaleChance(saleChance);
 	}
 	
+	//按条件查询销售机会（分页）
 	@Override
 	public List<SaleChance> selectSaleChancePaging(SaleChance saleChance){
 		return saleChanceMapper.selectSaleChancePaging(saleChance);
 	}
 	
+	//查询出下一个新增销售机会的Id 用于新增销售机会组件的id显示
 	@Override
 	public int selectLastChanceId() {
 		return saleChanceMapper.selectLastChanceId();
