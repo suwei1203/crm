6ace25de36de22d26c5d4abb451d4a99c11acbaf
zhouxiaoxu1996 <59716422+zhouxiaoxu1996@users.noreply.github.com>
2020-01-15 13:28:46 +0800

    Create ClientInfoMapper.java

---
+++ src/main/java/com/neusoft/mapper/custsw/ClientInfoMapper.java
@@ -0,0 +1,20 @@
+package com.neusoft.mapper.custsw;
+
+import com.neusoft.entity.ClientInfo;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+//客户信息管理
+@Mapper
+public interface ClientInfoMapper {
+	// 编辑客户信息,客户经理可以编辑状态为“正常”的客户信息
+	public int updateClientInfo(ClientInfo clientInfo);
+
+	// 查询全部客户信息
+	public List<ClientInfo> selectClientInfoAll();
+
+	// 查询六个月没有下订单的客户信息
+	public List<ClientInfo> selectClientInofByOrdersDate();
+}
