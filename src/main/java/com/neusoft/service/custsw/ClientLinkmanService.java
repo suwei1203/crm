9989d568bf60838bd51c77c679905049ae0bf215
zhouxiaoxu1996 <59716422+zhouxiaoxu1996@users.noreply.github.com>
2020-01-15 13:29:42 +0800

    Create ClientLinkmanService.java

---
+++ src/main/java/com/neusoft/service/custsw/ClientLinkmanService.java
@@ -0,0 +1,16 @@
+package com.neusoft.service.custsw;
+
+import java.util.List;
+
+import com.neusoft.entity.ClientLinkman;
+
+public interface ClientLinkmanService {
+	// 增加客户的联系人
+	public int insertClientLinkMan(ClientLinkman clientLinkman);
+
+	// 修改客户的联系人
+	public int updateClientLinkMan(ClientLinkman clientLinkman);
+
+	// 查询客户的全部联系人
+	public List<ClientLinkman> selectClientLinkmanAll(Integer clientCode);
+}
