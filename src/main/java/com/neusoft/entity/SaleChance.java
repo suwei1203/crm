23de5200175c1496682e1dae65bf6b719d75745f
zhouxiaoxu1996 <59716422+zhouxiaoxu1996@users.noreply.github.com>
2020-01-15 13:28:04 +0800

    Update SaleChance.java

--- src/main/java/com/neusoft/entity/SaleChance.java
+++ src/main/java/com/neusoft/entity/SaleChance.java
@@ -1,114 +1,154 @@
 package com.neusoft.entity;
 
 public class SaleChance {
-
+	// 销售机会
+	// 销售机会编号
 	private Integer chanceId;
+	// 客户名称
 	private String chanceCustName;
+	// 机会来源
 	private String chanceSource;
+	// 概要
 	private String chanceTitle;
+	// 成功机率(从0到100的数字)
 	private Integer chanceRate;
+	// 联系人
 	private String chanceLinkman;
+	// 联系人电话
 	private String chanceTel;
+	// 机会描述
 	private String chanceDesc;
+	// 创建人编号
 	private Integer chanceCreateId;
+	// 创建日期
 	private String chanceCreateDate;
+	// 指派人编号
 	private Integer chanceDueId;
+	// 指派日期
 	private String chanceDueDate;
+	// 销售机会状态:0:未分配1:开发中2:开发成3:开发失败
 	private Integer chanceStatus;
 	private Integer beginNum;
 	private Integer maxPageNum;
 
-		
 	public Integer getChanceId() {
 		return chanceId;
 	}
+
 	public void setChanceId(Integer chanceId) {
 		this.chanceId = chanceId;
 	}
+
 	public String getChanceSource() {
 		return chanceSource;
 	}
+
 	public void setChanceSource(String chanceSource) {
 		this.chanceSource = chanceSource;
 	}
+
 	public String getChanceCustName() {
 		return chanceCustName;
 	}
+
 	public void setChanceCustName(String chanceCustName) {
 		this.chanceCustName = chanceCustName;
 	}
+
 	public String getChanceTitle() {
 		return chanceTitle;
 	}
+
 	public void setChanceTitle(String chanceTitle) {
 		this.chanceTitle = chanceTitle;
 	}
+
 	public Integer getChanceRate() {
 		return chanceRate;
 	}
+
 	public void setChanceRate(Integer chanceRate) {
 		this.chanceRate = chanceRate;
 	}
+
 	public String getChanceLinkman() {
 		return chanceLinkman;
 	}
+
 	public void setChanceLinkman(String chanceLinkman) {
 		this.chanceLinkman = chanceLinkman;
 	}
+
 	public String getChanceTel() {
 		return chanceTel;
 	}
+
 	public void setChanceTel(String chanceTel) {
 		this.chanceTel = chanceTel;
 	}
+
 	public String getChanceDesc() {
 		return chanceDesc;
 	}
+
 	public void setChanceDesc(String chanceDesc) {
 		this.chanceDesc = chanceDesc;
 	}
+
 	public Integer getChanceCreateId() {
 		return chanceCreateId;
 	}
+
 	public void setChanceCreateId(Integer chanceCreateId) {
 		this.chanceCreateId = chanceCreateId;
 	}
+
 	public String getChanceCreateDate() {
 		return chanceCreateDate;
 	}
+
 	public void setChanceCreateDate(String chanceCreateDate) {
 		this.chanceCreateDate = chanceCreateDate;
 	}
+
 	public Integer getChanceDueId() {
 		return chanceDueId;
 	}
+
 	public void setChanceDueId(Integer chanceDueId) {
 		this.chanceDueId = chanceDueId;
 	}
+
 	public String getChanceDueDate() {
 		return chanceDueDate;
 	}
+
 	public void setChanceDueDate(String chanceDueDate) {
 		this.chanceDueDate = chanceDueDate;
 	}
+
 	public Integer getChanceStatus() {
 		return chanceStatus;
 	}
+
 	public void setChanceStatus(Integer chanceStatus) {
 		this.chanceStatus = chanceStatus;
 	}
+
 	public Integer getBeginNum() {
 		return beginNum;
 	}
+
 	public Integer getMaxPageNum() {
 		return maxPageNum;
 	}
+
 	public void setBeginNum(Integer beginNum) {
 		this.beginNum = beginNum;
 	}
+
 	public void setMaxPageNum(Integer maxPageNum) {
 		this.maxPageNum = maxPageNum;
 	}
-	
-	
+
 }
