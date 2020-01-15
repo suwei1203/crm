d37851a0189f5e8267f4e69a52e93d0ad878f283
suwei1203 <59653446+suwei1203@users.noreply.github.com>
2020-01-15 22:02:48 +0800

    Create ClientLinkmanService.java

---
+++ src/main/java/com/neusoft/service/salesw/ClientLinkmanService.java
@@ -0,0 +1,9 @@
+package com.neusoft.service.salesw;
+
+import com.neusoft.entity.ClientLinkman;
+
+public interface ClientLinkmanService {
+	
+	//添加联系人信息
+	public int insertClientLinkman(ClientLinkman clientLinkman);
+}
