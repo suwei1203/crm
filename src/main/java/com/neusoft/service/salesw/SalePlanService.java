0e2b1518362ed4e88a13ed316c1b407b9bc7774a
suwei1203 <59653446+suwei1203@users.noreply.github.com>
2020-01-15 13:17:22 +0800

    Update SalePlanService.java

--- src/main/java/com/neusoft/service/salesw/SalePlanService.java
+++ src/main/java/com/neusoft/service/salesw/SalePlanService.java
@@ -6,12 +6,16 @@ import com.neusoft.entity.SalePlan;
 
 public interface SalePlanService {
 	
+	//添加销售计划
 	public int insertSalePlan(SalePlan SalePlan);
 	
-	public List<SalePlan> selectSalePlanByPlanChcId(SalePlan SalePlan);
+	//动态条件（销售机会Id或销售计划Id）查询所属计划
+	public List<SalePlan> selectSalePlanByCondition(SalePlan SalePlan);
 	
+	//删除销售计划
 	public int delSalePlan(SalePlan SalePlan);
 	
+	//修改销售计划
 	public int updateSalePlan(SalePlan SalePlan);
 	
 }
