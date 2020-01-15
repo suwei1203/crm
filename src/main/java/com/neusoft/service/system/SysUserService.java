57fde3419551fd8e9c0d4827aff8f898bcff61a2
suwei1203 <59653446+suwei1203@users.noreply.github.com>
2020-01-15 13:17:36 +0800

    Update SysUserService.java

--- src/main/java/com/neusoft/service/system/SysUserService.java
+++ src/main/java/com/neusoft/service/system/SysUserService.java
@@ -6,7 +6,9 @@ import com.neusoft.entity.SysUser;
 
 public interface SysUserService {
 	
+	//根据id 密码查询系统用户
 	public SysUser selectSysUserByuserNameByuserPassword(SysUser sysUser);
 	
+	//根据条件 userRoleId（用户职责）查询 系统用户
 	public List<SysUser> selectSysUserByCondition(SysUser sysUser);
 }
