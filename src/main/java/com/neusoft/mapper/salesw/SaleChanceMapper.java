29edbe6b4f2d77ce7d71159e55cbccbecb7b6db6
suwei1203 <59653446+suwei1203@users.noreply.github.com>
2020-01-15 13:16:39 +0800

    Update SaleChanceMapper.java

--- src/main/java/com/neusoft/mapper/salesw/SaleChanceMapper.java
+++ src/main/java/com/neusoft/mapper/salesw/SaleChanceMapper.java
@@ -10,20 +10,27 @@ import com.neusoft.entity.SaleChance;
 
 @Mapper
 public interface SaleChanceMapper {
-
+	
+	//添加销售机会
 	public int insertSaleChance(SaleChance saleChance);
-
+	
+	// 按条件查询销售机会总行数
 	public int selectSaleChanceCount(SaleChance saleChance);
-
+	
+	// 按ID查询销售机会
 	@Select("select * from  saleChance where chanceId=#{chanceId}")
 	public SaleChance selectSaleChanceByChanceId(SaleChance saleChance);
-
+	
+	//修改销售机会
 	public int updateSaleChance(SaleChance saleChance);
-
+	
+	//删除销售机会
 	@Delete("delete from saleChance where chanceId=#{chanceId}")
 	public int delSaleChance(SaleChance saleChance);
-
+	
+	//按条件查询销售机会（分页）
 	public List<SaleChance> selectSaleChancePaging(SaleChance saleChance);
-
+	
+	//查询出下一个新增销售机会的Id 用于新增销售机会组件的id显示
 	public int selectLastChanceId();
 }
