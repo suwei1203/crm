622b2eb7d9eed8557c6af882692eb24c65ce532e
zhouxiaoxu1996 <59716422+zhouxiaoxu1996@users.noreply.github.com>
2020-01-15 13:29:55 +0800

    Create ClientInfoServiceImpl.java

---
+++ src/main/java/com/neusoft/service/custsw/impl/ClientInfoServiceImpl.java
@@ -0,0 +1,30 @@
+package com.neusoft.service.custsw.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.neusoft.entity.ClientInfo;
+import com.neusoft.mapper.custsw.ClientInfoMapper;
+import com.neusoft.service.custsw.ClientInfoService;
+
+public class ClientInfoServiceImpl implements ClientInfoService {
+	@Autowired
+	ClientInfoMapper clientInfoMapper;
+	// 编辑客户信息,客户经理可以编辑状态为“正常”的客户信息
+	@Override
+	public int updateClientInfo(ClientInfo clientInfo) {
+		return clientInfoMapper.updateClientInfo(clientInfo);
+	}
+	// 查询全部客户信息
+	@Override
+	public List<ClientInfo> selectClientInfoAll() {
+		return clientInfoMapper.selectClientInfoAll();
+	}
+	// 查询六个月没有下订单的客户信息
+	@Override
+	public List<ClientInfo> selectClientInofByOrdersDate() {
+		return clientInfoMapper.selectClientInofByOrdersDate();
+	}
+
+}
