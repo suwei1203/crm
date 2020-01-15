77c0b57bdd880109aecc54b2ede236af05927548
suwei1203 <59653446+suwei1203@users.noreply.github.com>
2020-01-15 13:16:47 +0800

    Update SalePlanMapper.java

--- src/main/java/com/neusoft/mapper/salesw/SalePlanMapper.java
+++ src/main/java/com/neusoft/mapper/salesw/SalePlanMapper.java
@@ -1,21 +1,22 @@
 package com.neusoft.mapper.salesw;
 
-
 import java.util.List;
-
 import org.apache.ibatis.annotations.Mapper;
 import com.neusoft.entity.SalePlan;
 
 @Mapper
 public interface SalePlanMapper {
-
+	
+	//添加销售计划
 	public int insertSalePlan(SalePlan SalePlan);
 	
-	//根据销售机会Id查询所属计划
-	public List<SalePlan> selectSalePlanByPlanChcId(SalePlan SalePlan);
+	//动态条件（销售机会Id或销售计划Id）查询所属计划
+	public List<SalePlan> selectSalePlanByCondition(SalePlan SalePlan);
 	
+	//删除销售计划
 	public int delSalePlan(SalePlan SalePlan);
 	
+	//修改销售计划
 	public int updateSalePlan(SalePlan SalePlan);
 
 }
