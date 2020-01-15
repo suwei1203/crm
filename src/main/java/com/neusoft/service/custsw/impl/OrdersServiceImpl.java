184fbc33dfef70c4abdb12db892fa5a6e58c3071
suwei1203 <59653446+suwei1203@users.noreply.github.com>
2020-01-15 14:15:20 +0800

    Update OrdersServiceImpl.java

--- src/main/java/com/neusoft/service/custsw/impl/OrdersServiceImpl.java
+++ src/main/java/com/neusoft/service/custsw/impl/OrdersServiceImpl.java
@@ -2,11 +2,17 @@ package com.neusoft.service.custsw.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.neusoft.entity.Orders;
 import com.neusoft.mapper.custsw.OrdersMapper;
 import com.neusoft.service.custsw.OrdersService;
 
+@Service
 public class OrdersServiceImpl implements OrdersService {
+	
+	@Autowired
 	OrdersMapper ordersMapper;
 
 	// 查询客户的所有订单
