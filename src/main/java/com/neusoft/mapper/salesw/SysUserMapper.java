21ca6175c930297597e5a942176b150dbdda4ed0
suwei1203 <59653446+suwei1203@users.noreply.github.com>
2020-01-15 16:57:03 +0800

    Update SysUserMapper.java

--- src/main/java/com/neusoft/mapper/salesw/SysUserMapper.java
+++ src/main/java/com/neusoft/mapper/salesw/SysUserMapper.java
@@ -11,7 +11,6 @@ public interface SysUserMapper {
 	
 	//根据id 密码查询系统用户
 	public SysUser selectSysUserByuserNameByuserPassword(SysUser sysUser);
-	
 	//根据条件 userRoleId（用户职责）查询 系统用户
 	public List<SysUser> selectSysUserByCondition(SysUser sysUser);
 	
