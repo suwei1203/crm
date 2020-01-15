4089d57f354e1c6ad73c83c7b2f6aad04294d716
suwei1203 <59653446+suwei1203@users.noreply.github.com>
2020-01-15 13:16:20 +0800

    Update SaleChanceController.java

--- src/main/java/com/neusoft/controller/salesw/SaleChanceController.java
+++ src/main/java/com/neusoft/controller/salesw/SaleChanceController.java
@@ -16,36 +16,43 @@ public class SaleChanceController {
 	@Autowired
 	SaleChanceService  saleChanceService;
 	
+	//添加销售机会
 	@RequestMapping("/insertSaleChance")
 	public int insertSaleChance(@RequestBody SaleChance saleChance) {
 		return saleChanceService.insertSaleChance(saleChance);
 	}
 	
+	// 按条件查询销售机会总行数
 	@RequestMapping("/selectSaleChanceCount")
 	public int selectSaleChanceCount(@RequestBody SaleChance saleChance) {
 		return saleChanceService.selectSaleChanceCount(saleChance);
 	}
 		
+	// 按ID查询销售机会
 	@RequestMapping("/selectSaleChanceByChanceId")
 	public SaleChance selectSaleChanceByChanceId(@RequestBody SaleChance saleChance) {
 		return saleChanceService.selectSaleChanceByChanceId(saleChance);
 	}
 	
+	//修改销售机会
 	@RequestMapping("/updateSaleChance")
 	public int updateSaleChance(@RequestBody SaleChance saleChance) {
 		return saleChanceService.updateSaleChance(saleChance);
 	}
 	
+	//删除销售机会
 	@RequestMapping("/delSaleChance")
 	public int delSaleChance(@RequestBody SaleChance saleChance) {
 		return saleChanceService.delSaleChance(saleChance);
 	}
 	
+	//按条件查询销售机会（分页）
 	@RequestMapping("/selectSaleChancePaging")
 	public List<SaleChance> selectSaleChancePaging(@RequestBody SaleChance saleChance){
 		return saleChanceService.selectSaleChancePaging(saleChance);
 	}
 	
+	//查询出下一个新增销售机会的Id 用于新增销售机会组件的id显示
 	@RequestMapping("/selectLastChanceId")
 	public int selectLastChanceId(){
 		return saleChanceService.selectLastChanceId();
