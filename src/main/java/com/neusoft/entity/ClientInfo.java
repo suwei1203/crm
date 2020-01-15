93be9b031c4368e28f94304f6cb9d52770238fb6
zhouxiaoxu1996 <59716422+zhouxiaoxu1996@users.noreply.github.com>
2020-01-15 13:27:45 +0800

    Update ClientInfo.java

--- src/main/java/com/neusoft/entity/ClientInfo.java
+++ src/main/java/com/neusoft/entity/ClientInfo.java
@@ -1,196 +1,235 @@
 package com.neusoft.entity;
 
 public class ClientInfo {
-	//客户编号（KH + 六位日期数字 + 三位数字流水号）
+	// 客户信息表
+	// 客户编号（KH + 六位日期数字 + 三位数字流水号）
 	private String clientCode;
-	//客户名称
+	// 客户名称
 	private String clientName;
-	//客户所在地区
+	// 客户所在地区:1,北京 2,华北 3,中南 4,东北 5,西部
 	private Integer clientAreaId;
-	//所属客户经理编号
+	// 所属客户经理编号
 	private Integer clientCustId;
-	//客户等级
+	// 客户等级:1、普通客户 2、大客户 3、合作伙伴 4、战略合作伙伴
 	private Integer clientLevelId;
-	//客户满意度
+	// 客户满意度:1、☆ 2、☆☆ 3、☆☆☆ 4、☆☆☆☆ 5、☆☆☆☆☆
 	private Integer clientContentment;
-	//客户信用度
+	// 客户信用度:1、☆ 2、☆☆ 3、☆☆☆ 4、☆☆☆☆ 5、☆☆☆☆☆
 	private Integer clientCredit;
-	//客户地址
+	// 客户地址
 	private String clientAddress;
-	//客户邮政编码
+	// 客户邮政编码
 	private String clientDakCode;
-	//电话
+	// 电话
 	private String clientTel;
-	//传真
+	// 传真
 	private String clientFax;
-	//网址
+	// 网址
 	private String clientNet;
-	//营业执照注册号
+	// 营业执照注册号
 	private String clientCharter;
-	//法人
+	// 法人
 	private String clientCorporation;
-	//注册资金（万元）
+	// 注册资金（万元）
 	private Integer clientBankroll;
-	//年营业额（万元）
+	// 年营业额（万元）
 	private Integer clientTurnoverYear;
-	//开户银行
+	// 开户银行
 	private String clientBank;
-	//银行帐号
+	// 银行帐号
 	private String clientAccounts;
-	//地税登记号
+	// 地税登记号
 	private String clientNativeTax;
-	//国税登记号
+	// 国税登记号
 	private String clientCountryTax;
-	//客户状态
+	// 客户状态:1、正常 2、暂缓流失 3、已流失.注意：只有在暂缓流失状态下，才可以转化为确认流失状态。
 	private Integer clientState;
-	//客户流失原因
+	// 客户流失原因
 	private String clientLoseReason;
-	//客户流失日期
+	// 客户流失日期
 	private String clientLoseDate;
-	
+
 	public String getClientCode() {
 		return clientCode;
 	}
+
 	public void setClientCode(String clientCode) {
 		this.clientCode = clientCode;
 	}
+
 	public String getClientName() {
 		return clientName;
 	}
+
 	public void setClientName(String clientName) {
 		this.clientName = clientName;
 	}
+
 	public Integer getClientAreaId() {
 		return clientAreaId;
 	}
+
 	public void setClientAreaId(Integer clientAreaId) {
 		this.clientAreaId = clientAreaId;
 	}
+
 	public Integer getClientCustId() {
 		return clientCustId;
 	}
+
 	public void setClientCustId(Integer clientCustId) {
 		this.clientCustId = clientCustId;
 	}
+
 	public Integer getClientLevelId() {
 		return clientLevelId;
 	}
+
 	public void setClientLevelId(Integer clientLevelId) {
 		this.clientLevelId = clientLevelId;
 	}
+
 	public Integer getClientContentment() {
 		return clientContentment;
 	}
+
 	public void setClientContentment(Integer clientContentment) {
 		this.clientContentment = clientContentment;
 	}
+
 	public Integer getClientCredit() {
 		return clientCredit;
 	}
+
 	public void setClientCredit(Integer clientCredit) {
 		this.clientCredit = clientCredit;
 	}
+
 	public String getClientAddress() {
 		return clientAddress;
 	}
+
 	public void setClientAddress(String clientAddress) {
 		this.clientAddress = clientAddress;
 	}
+
 	public String getClientDakCode() {
 		return clientDakCode;
 	}
+
 	public void setClientDakCode(String clientDakCode) {
 		this.clientDakCode = clientDakCode;
 	}
+
 	public String getClientTel() {
 		return clientTel;
 	}
+
 	public void setClientTel(String clientTel) {
 		this.clientTel = clientTel;
 	}
+
 	public String getClientFax() {
 		return clientFax;
 	}
+
 	public void setClientFax(String clientFax) {
 		this.clientFax = clientFax;
 	}
+
 	public String getClientNet() {
 		return clientNet;
 	}
+
 	public void setClientNet(String clientNet) {
 		this.clientNet = clientNet;
 	}
+
 	public String getClientCharter() {
 		return clientCharter;
 	}
+
 	public void setClientCharter(String clientCharter) {
 		this.clientCharter = clientCharter;
 	}
+
 	public String getClientCorporation() {
 		return clientCorporation;
 	}
+
 	public void setClientCorporation(String clientCorporation) {
 		this.clientCorporation = clientCorporation;
 	}
+
 	public Integer getClientBankroll() {
 		return clientBankroll;
 	}
+
 	public void setClientBankroll(Integer clientBankroll) {
 		this.clientBankroll = clientBankroll;
 	}
+
 	public Integer getClientTurnoverYear() {
 		return clientTurnoverYear;
 	}
+
 	public void setClientTurnoverYear(Integer clientTurnoverYear) {
 		this.clientTurnoverYear = clientTurnoverYear;
 	}
+
 	public String getClientBank() {
 		return clientBank;
 	}
+
 	public void setClientBank(String clientBank) {
 		this.clientBank = clientBank;
 	}
+
 	public String getClientAccounts() {
 		return clientAccounts;
 	}
+
 	public void setClientAccounts(String clientAccounts) {
 		this.clientAccounts = clientAccounts;
 	}
+
 	public String getClientNativeTax() {
 		return clientNativeTax;
 	}
+
 	public void setClientNativeTax(String clientNativeTax) {
 		this.clientNativeTax = clientNativeTax;
 	}
+
 	public String getClientCountryTax() {
 		return clientCountryTax;
 	}
+
 	public void setClientCountryTax(String clientCountryTax) {
 		this.clientCountryTax = clientCountryTax;
 	}
+
 	public Integer getClientState() {
 		return clientState;
 	}
+
 	public void setClientState(Integer clientState) {
 		this.clientState = clientState;
 	}
+
 	public String getClientLoseReason() {
 		return clientLoseReason;
 	}
+
 	public void setClientLoseReason(String clientLoseReason) {
 		this.clientLoseReason = clientLoseReason;
 	}
+
 	public String getClientLoseDate() {
 		return clientLoseDate;
 	}
+
 	public void setClientLoseDate(String clientLoseDate) {
 		this.clientLoseDate = clientLoseDate;
 	}
-	
-	
-	
-	
-	
-	
-	
 }
