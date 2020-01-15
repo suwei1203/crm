db4d5dd8e6f4fceeeff5d5f0213ffa702451ef81
suwei1203 <59653446+suwei1203@users.noreply.github.com>
2020-01-15 14:15:36 +0800

    Delete ClientLinkmanService.java

--- src/main/java/com/neusoft/service/salesw/ClientLinkmanService.java
+++
@@ -1,9 +0,0 @@
-package com.neusoft.service.salesw;
-
-import com.neusoft.entity.ClientLinkman;
-
-public interface ClientLinkmanService {
-	
-	//添加联系人信息
-	public int insertClientLinkman(ClientLinkman clientLinkman);
-}
