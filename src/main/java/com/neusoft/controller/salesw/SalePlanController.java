e4a31d9f748a958ad93cc69f3a99df5b546ded32
suwei1203 <59653446+suwei1203@users.noreply.github.com>
2020-01-15 13:16:25 +0800

    Update SalePlanController.java

--- src/main/java/com/neusoft/controller/salesw/SalePlanController.java
+++ src/main/java/com/neusoft/controller/salesw/SalePlanController.java
@@ -16,21 +16,25 @@ public class SalePlanController {
 	@Autowired
 	SalePlanService salePlanService;
 
+	//添加销售计划
 	@RequestMapping("/insertSalePlan")
 	public int insertSalePlan(@RequestBody SalePlan SalePlan) {
 		return salePlanService.insertSalePlan(SalePlan);
 	}
 	
-	@RequestMapping("/selectSalePlanByPlanChcId")
-	public List<SalePlan> selectSalePlanByPlanChcId(@RequestBody SalePlan SalePlan){
-		return salePlanService.selectSalePlanByPlanChcId(SalePlan);
+	//动态条件（销售机会Id或销售计划Id）查询所属计划
+	@RequestMapping("/selectSalePlanByCondition")
+	public List<SalePlan> selectSalePlanByCondition(@RequestBody SalePlan SalePlan){
+		return salePlanService.selectSalePlanByCondition(SalePlan);
 	}
 	
+	//删除销售计划
 	@RequestMapping("/delSalePlan")
 	public int delSalePlan(@RequestBody  SalePlan SalePlan) {
 		return salePlanService.delSalePlan(SalePlan);
 	}
 	
+	//修改销售计划
 	@RequestMapping("/updateSalePlan")
 	public int updateSalePlan(@RequestBody SalePlan SalePlan) {
 		return salePlanService.updateSalePlan(SalePlan);
