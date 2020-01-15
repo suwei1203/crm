15876e68a9281256a8b21ee5036d23c0b259fd8f
suwei1203 <59653446+suwei1203@users.noreply.github.com>
2020-01-15 14:14:07 +0800

    Update ClientReprieveLoseMapper.java

--- src/main/java/com/neusoft/mapper/custsw/ClientReprieveLoseMapper.java
+++ src/main/java/com/neusoft/mapper/custsw/ClientReprieveLoseMapper.java
@@ -1,8 +1,11 @@
 package com.neusoft.mapper.custsw;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.neusoft.entity.ClientInfo;
 import com.neusoft.entity.ClientReprieveLose;
 
+@Mapper
 public interface ClientReprieveLoseMapper {
 //	客户暂缓流失
 	public int insertClientReprieveLose(ClientReprieveLose clientReprieveLose);
