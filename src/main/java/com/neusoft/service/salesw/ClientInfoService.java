0408dfc0dafc1bb4bfca56a035e9327aa2c14b8a
suwei1203 <59653446+suwei1203@users.noreply.github.com>
2020-01-16 12:45:33 +0800

    Delete ClientInfoService.java

--- src/main/java/com/neusoft/service/salesw/ClientInfoService.java
+++
@@ -1,9 +0,0 @@
-package com.neusoft.service.salesw;
-
-import com.neusoft.entity.ClientInfo;
-
-public interface ClientInfoService {
-	
-	//添加客户信息
-	public int insertClientInfo(ClientInfo clientInfo);
-}
