340ff7e3b5096a36945b7f136ba97dbd56891695
suwei1203 <59653446+suwei1203@users.noreply.github.com>
2020-01-16 11:45:52 +0800

    Create ClientLinkmanServiceImpl.java

---
+++ src/main/java/com/neusoft/service/custsw/impl/ClientLinkmanServiceImpl.java
@@ -0,0 +1,36 @@
+package com.neusoft.service.custsw.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.neusoft.entity.ClientLinkman;
+import com.neusoft.mapper.custsw.ClientLinkmanMapper;
+import com.neusoft.service.custsw.ClientLinkmanService;
+
+@Service
+public class ClientLinkmanServiceImpl implements ClientLinkmanService {
+	@Autowired
+	ClientLinkmanMapper clientLinkmanMapper;
+
+	// 增加客户的联系人
+	@Override
+	public int insertClientLinkMan(ClientLinkman clientLinkman) {
+		return clientLinkmanMapper.insertClientLinkman(clientLinkman);
+	}
+
+	// 修改客户的联系人
+	@Override
+	public int updateClientLinkMan(ClientLinkman clientLinkman) {
+		return clientLinkmanMapper.updateClientLinkman(clientLinkman);
+	}
+
+	// 查询客户的全部联系人
+	@Override
+	public List<ClientLinkman> selectClientLinkmanAll(Integer clientCode) {
+		return clientLinkmanMapper.selectClientLinkmanAll(clientCode);
+	}
+
+	
+}
